729993540bcd1c29973981b4569793c28ebbed6b
Siddharth_paul <paulsiddharth000@gmail.com>
2026-03-06 16:03:07 +0530

    isseue 49 (#142)

--- src/SearchEngine.cpp
+++ src/SearchEngine.cpp
@@ -62,11 +62,19 @@ std::vector<Book*> SearchEngine::searchByAuthor(const std::string& query) const 
 
 std::vector<Book*> SearchEngine::filterByGenre(const std::string& genre) const {
     std::vector<Book*> results;
+
+    std::string q = genre;
+    std::transform(q.begin(), q.end(), q.begin(), ::tolower);
+
     for (auto& book : catalog_) {
-        if (book.getGenre() == genre) {
+        std::string g = book.getGenre();
+        std::transform(g.begin(), g.end(), g.begin(), ::tolower);
+
+        if (g == q) {
             results.push_back(const_cast<Book*>(&book));
         }
     }
+
     return results;
 }
 
